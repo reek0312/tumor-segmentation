fixed image scaling

segment&visualize.py
@@ -30,7 +30,7 @@ def preprocess_image(image_path):
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (256, 256))
     img = img / 255.0
-    img = (img - 0.5) / 0.5 #to match the training scaling
+    img = (img - 0.5) #to match the training scaling
     img = np.expand_dims(img, axis=0)
     img = np.expand_dims(img, axis=0)
     return torch.tensor(img, dtype=torch.float32)